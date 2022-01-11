b73d3900c6bc3c1955ff670f2f4f7d83fcbe0d67
imarcosenciso <imarcosenciso@opendeusto.es>
2022-01-11 17:44:00 +0100

    functions added + loggin (yet to work)

--- Final/client.c
+++ Final/client.c
@@ -6,11 +6,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include "client.h"
 
-#define BUFFER_SIZE 1024
 #define RESPONSE_LEN 9 // HH:MM:SS\0
-
-#define DEFAULT_PORT 4242
 
 int main(int argc, char *argv[])
 {
@@ -19,6 +17,7 @@ int main(int argc, char *argv[])
 
     struct sockaddr_in srv_addr;
     char buffer[BUFFER_SIZE];
+    char username[PARAMS_MAX_LENGHT + 1];
 
     char *cmd = "REQ\0";
 
@@ -27,23 +26,9 @@ int main(int argc, char *argv[])
 
     // Comando da inviare al server
 
-    /* Creazione socket */
-    sd = socket(AF_INET, SOCK_STREAM, 0);
+    socket_creation(port, &sd, &srv_addr);
 
-    /* Creazione indirizzo del server */
-    memset(&srv_addr, 0, sizeof(srv_addr));
-    srv_addr.sin_family = AF_INET;
-    srv_addr.sin_port = htons(port);
-    inet_pton(AF_INET, "127.0.0.1", &srv_addr.sin_addr);
-
-    /* connessione */
-    ret = connect(sd, (struct sockaddr *)&srv_addr, sizeof(srv_addr));
-
-    if (ret < 0)
-    {
-        perror("Errore in fase di connessione: \n");
-        exit(1);
-    }
+    stablish_connection(&ret, &sd, &srv_addr);
 
     for (;;)
     {
@@ -71,4 +56,61 @@ int main(int argc, char *argv[])
         sleep(5);
 
     } //chiudo il "for sempre" (linea 40)
+}
+
+void socket_creation(int port, int *sd, struct sockaddr_in *srv_addr)
+{
+    /* Creazione socket */
+    *sd = socket(AF_INET, SOCK_STREAM, 0);
+
+    /* Creazione indirizzo del server */
+    memset(srv_addr, 0, sizeof(*srv_addr));
+    srv_addr->sin_family = AF_INET;
+    srv_addr->sin_port = htons(port);
+    inet_pton(AF_INET, "127.0.0.1", &srv_addr->sin_addr);
+}
+
+void stablish_connection(int *ret, int *sd, struct sockaddr_in *srv_addr)
+{
+    *ret = connect(*sd, (struct sockaddr *)srv_addr, sizeof(*srv_addr));
+
+    if (*ret < 0)
+    {
+        perror("Error during connection to server: \n");
+        exit(1);
+    }
+
+    printf("Succesfully connected to Server!\n");
+}
+
+void initial_menu()
+{
+    char instruction[10], username[PARAMS_MAX_LENGHT + 1], password[PARAMS_MAX_LENGHT + 1];
+    int srv_port;
+    printf("User has to sign up if they don't have an account. Can log in otherwise.\n"
+           "\tTo sign up, type: \"signup username password\"\n"
+           "\tTo log in, type \"in srv_port username password\n");
+
+    while (1)
+    {
+        printf("Command: ");
+        scanf("%s", instruction);
+        if (strcmp(instruction, "signup "))
+        {
+            scanf("%s", username);
+            scanf("%s", password);
+            break;
+        }
+        else if (strcmp(instruction, "in "))
+        {
+            scanf("%d", srv_port);
+            scanf("%s", username);
+            scanf("%s", password);
+            break;
+        }
+        else
+        {
+            printf("\nUnkown command, please try again.");
+        }
+    }
 }
--- Final/client.h
+++ Final/client.h
@@ -1,11 +1,32 @@
-typedef int ins_type;
+// Some constants
+#define BUFFER_SIZE 1024
+#define DEFAULT_PORT 4242
+#define PARAMS_MAX_LENGHT 30
 
+// Structs and instruction type definition (same as an enumeration in Java)
 #define INS_SING_UP 1
 #define INS_SING_IN 2
 
+typedef int ins_type;
+
+// Define the structures for the different instructions or messages that are going to be sent.
 typedef struct ins_sing_up
 {
     ins_type ins_type; // INS_SING_UP
-    char user_dest;
-    char user_pass;
-};
+    char user_dest[PARAMS_MAX_LENGHT + 1];
+    char user_pass[PARAMS_MAX_LENGHT + 1];
+} ins_sing_up;
+
+typedef struct ins_sing_in
+{
+    ins_type ins_type; // = INS_SING_IN
+    char user_dest[PARAMS_MAX_LENGHT + 1];
+    char user_pass[PARAMS_MAX_LENGHT + 1];
+} ins_sing_in;
+
+// Function definition
+void socket_creation(int port, int *sd, struct sockaddr_in *srv_addr);
+void stablish_connection(int *ret, int *sd, struct sockaddr_in *srv_addr);
+
+// Functions related to the service
+char *signup();
